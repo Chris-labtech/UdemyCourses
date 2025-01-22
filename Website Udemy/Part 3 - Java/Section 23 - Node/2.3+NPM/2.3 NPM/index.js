a8765ad10e6e7cefca0360ae895242d2b9555e8f
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-01-22 20:40:46 +0200

    Update

---
+++ Website Udemy/Part 3 - Java/Section 23 - Node/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// var superheroes = generateSuperHeroes();
+
+import {randomSuperhero} from 'superheroes';
+
+randomSuperhero();
